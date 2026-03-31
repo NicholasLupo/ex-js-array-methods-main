0b13f062b3dc93da7bab87b9ff7d9eed8f7694a7
NicholasLupo <nicholaslupo14@gmail.com>
2026-03-31 15:45:47 +0200

    Complete snack-8

--- snack-8/script.js
+++ snack-8/script.js
@@ -21,3 +21,13 @@ const students = [
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
+
+const aula = students.filter((student) => {
+
+  if (student.name === 'Marco Lanci') {
+
+    console.log(student.class);
+
+  }
+
+})
