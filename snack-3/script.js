e7da5b2145ad2cfc47eaf2f19691391b36629af3
NicholasLupo <nicholaslupo14@gmail.com>
2026-03-31 14:55:01 +0200

    Complete snack-3

--- snack-3/script.js
+++ snack-3/script.js
@@ -2,3 +2,11 @@ const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
+
+const arrayNum = numbers.map((numb) => {
+
+    return numb + 1;
+
+})
+
+console.log(arrayNum);
